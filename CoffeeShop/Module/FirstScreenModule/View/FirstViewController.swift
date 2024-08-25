87dac7c0a2eed4e66f19dd6dd842a03ca791e5b3
AlexAlexBoga <AlexAlexBoga@users.noreply.github.com>
2024-08-25 12:47:54 +0300

    Add background image

---
+++ CoffeeShop/Module/FirstScreenModule/View/FirstViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FirstViewController.swift
+//  CoffeeShop
+//
+//  Created by Александр Богачев on 23.08.24.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    private let backGroundImage = UIInputView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    func setupBackGroundImage() {
+    
+    }
+}
