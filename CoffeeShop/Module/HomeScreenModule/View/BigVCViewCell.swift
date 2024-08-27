920d7b6fd2c6084c44134d16289e9630d954ad76
AlexAlexBoga <AlexAlexBoga@users.noreply.github.com>
2024-08-27 14:31:38 +0300

    Added Vertical CollectionView to HomeScreen

---
+++ CoffeeShop/Module/HomeScreenModule/View/BigVCViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  BigVCViewCell.swift
+//  CoffeeShop
+//
+//  Created by Александр Богачев on 27.08.24.
+//
+
+
+import UIKit
+
+class BigVCViewCell: UICollectionViewCell {
+    
+    let topView = UIView()
+    let titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCell() {
+        contentView.backgroundColor = .blue
+        setupTopView()
+        setupBottomLabel()
+    }
+    
+    func setupTopView() {
+        contentView.addSubview(topView)
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.layer.cornerRadius = 10
+         
+        topView.backgroundColor = .green
+        topView.layer.cornerRadius = 8
+        topView.layer.masksToBounds = true
+        
+        NSLayoutConstraint.activate([
+            topView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 256),
+            topView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            topView.widthAnchor.constraint(equalToConstant: 70),
+            topView.heightAnchor.constraint(equalToConstant: 93),
+        ])
+        
+    }
+    
+    func setupBottomLabel() {
+        contentView.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        titleLabel.text = "Title label"
+        titleLabel.textColor = .black
+    
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 15),
+        ])
+    }
+    
+}
--- CoffeeShop/Module/HomeScreenModule/View/HomeViewController.swift
+++ CoffeeShop/Module/HomeScreenModule/View/HomeViewController.swift
@@ -18,7 +18,20 @@ class HomeViewController: UIViewController {
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 8
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.tag = 1
 
+        return collection
+    }()
+    
+    lazy var bigVCollection: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 20
+        layout.minimumLineSpacing = 20
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.tag = 2
+        
         return collection
     }()
     
@@ -28,6 +41,7 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .gray
         setupLabel()
         setupSmallHCollection()
+        setupBigVCollection() 
         
     }
  
@@ -61,25 +75,71 @@ class HomeViewController: UIViewController {
         ])
     }
     
+    func setupBigVCollection() {
+        view.addSubview(bigVCollection)
+        bigVCollection.translatesAutoresizingMaskIntoConstraints = false
+        
+        bigVCollection.backgroundColor = .red
+        bigVCollection.delegate = self
+        bigVCollection.dataSource = self
+        bigVCollection.register(BigVCViewCell.self, forCellWithReuseIdentifier: "BigVCViewCell")
+        
+        NSLayoutConstraint.activate([
+            bigVCollection.topAnchor.constraint(equalTo: smallHCollection.bottomAnchor, constant: 50),
+            bigVCollection.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            bigVCollection.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+//            bigVCollection.heightAnchor.constraint(equalToConstant: 1000),
+            bigVCollection.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
 }
 
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-      10
+        switch collectionView.tag {
+        case 1:
+            return 10
+        case 2:
+            return 15
+        default:
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SmallHCViewCell", for: indexPath)
         
-        return cell
+        switch collectionView.tag {
+        case 1:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SmallHCViewCell", for: indexPath)
+            return cell
+        case 2:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BigVCViewCell", for: indexPath)
+            return cell
+        default:
+            return UICollectionViewCell()
+            
+        }
+        
     }
-    
 }
 
-extension HomeViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-      
-        return CGSize(width: 110, height: 24)
+    extension HomeViewController: UICollectionViewDelegateFlowLayout {
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            
+            switch collectionView.tag {
+            case 1:
+                return CGSize(width: 110, height: 24)
+                     case 2:
+//                let width = collectionView.bounds.width
+//                let height = 130.0
+//                return CGSize(width: width, height: height)
+                return CGSize(width: 332, height: 105)
+            default:
+                return CGSize(width: 0, height: 0)
+            }
+//            return CGSize(width: 110, height: 24)
+        }
     }
-}
+
